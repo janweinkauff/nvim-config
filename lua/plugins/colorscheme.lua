39598ada83286a96b86732c9a9c26248860b20d4
Jan Weinkauff <jan@weinkauff.cloud>
2023-12-01 09:24:51 +0100

    Let the moon shine 🌙

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("rose-pine").setup({})
+        require("rose-pine").setup({ variant = 'moon' })
         vim.cmd.colorscheme "rose-pine"
     end
 }
